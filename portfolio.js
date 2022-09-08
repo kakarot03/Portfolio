bedc8b0adb5f5e252840f41429726d4401c2cb8a
Srivarshan <srivarshan611@gmail.com>
2022-09-08 16:35:31 +0530

    Updated Work, Resume

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,10 @@
-// Show menu
-
 const showMenu = (toggleId, navID) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navID);
 
   if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      nav.classList.toggle("show");
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
     });
   }
 };
@@ -17,14 +15,14 @@ function dummy(e) {
 }
 
 function sendMail() {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("emailId").value;
-  let msg = document.getElementById("message").value;
-  let err = document.getElementById("error");
-  if (name === "" || email === "" || msg === "") {
-    err.classList.remove("success_message");
-    err.classList.add("error_message");
-    err.innerHTML = "Fill all the Sections";
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('emailId').value;
+  let msg = document.getElementById('message').value;
+  let err = document.getElementById('error');
+  if (name === '' || email === '' || msg === '') {
+    err.classList.remove('success_message');
+    err.classList.add('error_message');
+    err.innerHTML = 'Fill all the Sections';
     setTimeout(removeMsg, 6000);
     return;
   }
@@ -33,20 +31,20 @@ function sendMail() {
     email_id: email,
     message: msg,
   };
-  err.classList.remove("error_message");
-  err.innerHTML = "";
-  document.getElementById("contact_form").reset();
+  err.classList.remove('error_message');
+  err.innerHTML = '';
+  document.getElementById('contact_form').reset();
   emailjs
-    .send("service_5o6n3vc", "template_plrwrh5", params)
+    .send('service_5o6n3vc', 'template_plrwrh5', params)
     .then(function (res) {
-      err.classList.add("success_message");
-      err.innerHTML = "Mail Sent Successfully";
+      err.classList.add('success_message');
+      err.innerHTML = 'Mail Sent Successfully';
     });
   setTimeout(removeMsg, 6000);
 }
 
 function removeMsg() {
-  document.getElementById("error").innerHTML = "";
+  document.getElementById('error').innerHTML = '';
 }
 
-showMenu("nav_toggle", "nav_menu");
+showMenu('nav_toggle', 'nav_menu');
